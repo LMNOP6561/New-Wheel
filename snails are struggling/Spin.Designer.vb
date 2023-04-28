08b85be940d73541533f0ccc10a61d37fe1654b3
ehamilto6561 <ehamilto6561@W10-C821NL3>
2023-04-28 10:30:53 -0500

    Add project files.

---
+++ snails are struggling/Spin.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSpin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picCircle = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picCircle, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picCircle
+        '
+        Me.picCircle.Image = Global.snails_are_struggling.My.Resources.Resources.Untitled_design__20_
+        Me.picCircle.Location = New System.Drawing.Point(114, 37)
+        Me.picCircle.Name = "picCircle"
+        Me.picCircle.Size = New System.Drawing.Size(464, 365)
+        Me.picCircle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picCircle.TabIndex = 0
+        Me.picCircle.TabStop = False
+        '
+        'frmSpin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(723, 467)
+        Me.Controls.Add(Me.picCircle)
+        Me.Name = "frmSpin"
+        Me.Text = "Spin"
+        CType(Me.picCircle, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picCircle As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ snails are struggling/Spin.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmSpin
+
+    Private Sub snails(x)
+        Select Case x
+            Case = 0
+                picCircle.Image = My.Resources.Untitled_design__1_
+            Case = 1
+                picCircle.Image = My.Resources.Untitled_design__2_
+            Case = 2
+                picCircle.Image = My.Resources.Untitled_design__3_
+            Case = 3
+                picCircle.Image = My.Resources.Untitled_design__4_
+        End Select
+    End Sub
+    Private Sub frmSpin_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Timer1.Interval = 500
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim thing1 As Integer
+        thing1 = Int(Rnd() * 100 + 50)
+        Dim pictureval As Integer
+
+        For range = 0 To thing1
+            pictureval = pictureval Mod 25
+            snails(pictureval)
+        Next
+    End Sub
+End Class
