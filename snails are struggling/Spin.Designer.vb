8666a20c40c1e7f0c43884eb3c9a39679d75c82c
ehamilto6561 <ehamilto6561@W10-C821NL3>
2023-04-28 23:29:14 -0500

    v1.2

--- snails are struggling/Spin.Designer.vb
+++ snails are struggling/Spin.Designer.vb
@@ -25,6 +25,8 @@ Partial Class frmSpin
         Me.components = New System.ComponentModel.Container()
         Me.picCircle = New System.Windows.Forms.PictureBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         CType(Me.picCircle, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -38,19 +40,47 @@ Partial Class frmSpin
         Me.picCircle.TabIndex = 0
         Me.picCircle.TabStop = False
         '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Snails"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Bees"
+        '
         'frmSpin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(723, 467)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.picCircle)
         Me.Name = "frmSpin"
         Me.Text = "Spin"
         CType(Me.picCircle, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents picCircle As PictureBox
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- snails are struggling/Spin.vb
+++ snails are struggling/Spin.vb
@@ -1,38 +1,83 @@
 ﻿
 Public Class frmSpin
 
-    Dim y As Integer = 2
+    Dim y As Integer = 19
+    Dim z As Double
+
+
     Private Sub snails(x)
-        Select Case x
-            Case = 0
-                picCircle.Image = My.Resources.Untitled_design__1_
-            Case = 1
-                picCircle.Image = My.Resources.Untitled_design__2_
-            Case = 2
-                picCircle.Image = My.Resources.Untitled_design__3_
-            Case = 3
-                picCircle.Image = My.Resources.Untitled_design__4_
-        End Select
+        If x = 0 Then
+            picCircle.Image = My.Resources.Untitled_design
+        ElseIf x = 1 Then
+            picCircle.Image = My.Resources.Untitled_design__1_
+        ElseIf x = 2 Then
+            picCircle.Image = My.Resources.Untitled_design__2_
+        ElseIf x = 3 Then
+            picCircle.Image = My.Resources.Untitled_design__3_
+        ElseIf x = 4 Then
+            picCircle.Image = My.Resources.Untitled_design__4_
+        ElseIf x = 5 Then
+            picCircle.Image = My.Resources.Untitled_design__5_
+        ElseIf x = 6 Then
+            picCircle.Image = My.Resources.Untitled_design__6_
+        ElseIf x = 7 Then
+            picCircle.Image = My.Resources.Untitled_design__7_
+        ElseIf x = 8 Then
+            picCircle.Image = My.Resources.Untitled_design__8_
+        ElseIf x = 9 Then
+            picCircle.Image = My.Resources.Untitled_design__9_
+        ElseIf x = 10 Then
+            picCircle.Image = My.Resources.Untitled_design__10_
+        ElseIf x = 11 Then
+            picCircle.Image = My.Resources.Untitled_design__11_
+        ElseIf x = 12 Then
+            picCircle.Image = My.Resources.Untitled_design__12_
+        ElseIf x = 13 Then
+            picCircle.Image = My.Resources.Untitled_design__13_
+        ElseIf x = 14 Then
+            picCircle.Image = My.Resources.Untitled_design__14_
+        ElseIf x = 15 Then
+            picCircle.Image = My.Resources.Untitled_design__15_
+        ElseIf x = 16 Then
+            picCircle.Image = My.Resources.Untitled_design__16_
+        ElseIf x = 17 Then
+            picCircle.Image = My.Resources.Untitled_design__17_
+        ElseIf x = 18 Then
+            picCircle.Image = My.Resources.Untitled_design__18_
+        ElseIf x = 19 Then
+            picCircle.Image = My.Resources.Untitled_design__19_
+        ElseIf x = 20 Then
+            picCircle.Image = My.Resources.Untitled_design__20_
+        End If
 
     End Sub
     Private Sub frmSpin_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Timer1.Interval = 500
-        Dim z As Double
-        z = y Mod 2
-        Dim thing1 As Integer
-        thing1 = Int((Rnd() * 101) + 50)
-        Dim pictureval As Integer
-        For i As Integer = 0 To thing1
-            pictureval = i Mod 25
-            snails(pictureval)
-        Next
-
-
+        Timer1.Interval = 100
 
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         y += 1
+        Label1.Text = y.ToString
+        z = y Mod 2
+        Dim thing1 As Integer
+        thing1 = Int(Rnd() * 101 + 50)
+        Dim pictureval As Integer
+
+        pictureval = y Mod 20
+        snails(pictureval)
+        Label2.Text = pictureval.ToString
+
+
+
+
+        'If z = 0 Then
+        '    For i As Integer = 0 To thing1
+        '        pictureval = i Mod 25
+        '        snails(pictureval)
+        '        Label2.Text = pictureval.ToString
+        '    Next i
+        'End If
 
     End Sub
 End Class
