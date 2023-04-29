84c85fb7c1adddc90747a9618ec9ffc8a055e0f8
ehamilto6561 <ehamilto6561@W10-C821NL3>
2023-04-28 22:28:44 -0500

    v1.1

--- snails are struggling/Spin.vb
+++ snails are struggling/Spin.vb
@@ -1,5 +1,7 @@
-﻿Public Class frmSpin
+﻿
+Public Class frmSpin
 
+    Dim y As Integer = 2
     Private Sub snails(x)
         Select Case x
             Case = 0
@@ -11,20 +13,26 @@
             Case = 3
                 picCircle.Image = My.Resources.Untitled_design__4_
         End Select
+
     End Sub
     Private Sub frmSpin_Load(sender As Object, e As EventArgs) Handles Me.Load
         Timer1.Interval = 500
+        Dim z As Double
+        z = y Mod 2
+        Dim thing1 As Integer
+        thing1 = Int((Rnd() * 101) + 50)
+        Dim pictureval As Integer
+        For i As Integer = 0 To thing1
+            pictureval = i Mod 25
+            snails(pictureval)
+        Next
+
+
+
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        y += 1
 
-        Dim thing1 As Integer
-        thing1 = Int(Rnd() * 100 + 50)
-        Dim pictureval As Integer
-
-        For range = 0 To thing1
-            pictureval = pictureval Mod 25
-            snails(pictureval)
-        Next
     End Sub
 End Class
